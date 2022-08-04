8abccdecc12cd92134a0b14397b0b4a5d046e06a
Andrew Simmons <andrewkylesimmons@gmail.com>
2022-08-04 17:28:32 +1200

    Move main script to separate file

---
+++ entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ "${PROBLEM_MATCHERS}" = "*" ]; then
+    matchers="$(find "${GITHUB_ACTION_PATH}/matchers/" -name "*.json" -exec basename {} .json \;)"
+else
+    matchers="${PROBLEM_MATCHERS}"
+fi
+
+for matcher in ${matchers}; do
+    if [ "${ENABLE_PROBLEM_MATCHERS}" = "true" ]; then
+        path="${GITHUB_ACTION_PATH}/matchers/${matcher}.json"
+        echo "Adding problem matcher ${path}"
+        echo "::add-matcher::${path}"
+    else
+        echo "Removing problem matcher ${matcher}"
+        echo "::remove-matcher owner=${matcher}::"
+    fi
+done
